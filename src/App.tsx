e32e14f9883d4d660e6910401b0edc6ef1a224d9
Mehoff <mixov2000@gmail.com>
2022-03-17 21:55:45 +0200

    addTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 import TodoList from "./components/todo-list";
@@ -28,15 +28,58 @@ const TODOS: ITodo[] = [
 ];
 
 function App() {
+  const [header, setHeader] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
   const [todos, setTodos] = useState<ITodo[]>([]);
 
   useEffect(() => {
     setTodos(TODOS);
   }, []);
 
+  const addTodo = (): void => {
+    const todo: ITodo = {
+      header: header.trim(),
+      description: description.trim(),
+      isFinised: false,
+    };
+
+    setTodos([...todos, todo]);
+    setHeader("");
+    setDescription("");
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const text = e.target.value;
+    setHeader(text);
+  };
+
+  const onTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    const text = e.target.value;
+    setDescription(text);
+  };
+
   return (
     <div className={styles.App}>
       <TodoList todos={todos} />
+      <div>
+        <label htmlFor="header">Header</label>
+        <input
+          name="header"
+          id="todo-header"
+          placeholder="Bring Mary her book back"
+          value={header}
+          onChange={onInputChange}
+        ></input>
+        <label htmlFor="description">Description</label>
+        <textarea
+          name="description"
+          id="todo-description"
+          placeholder="It should be done before it is too late"
+          value={description}
+          onChange={onTextAreaChange}
+        ></textarea>
+        <button onClick={addTodo}>Add todo</button>
+      </div>
     </div>
   );
 }
